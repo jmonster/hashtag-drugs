91fce3e7a73c2a8f3124e1e3378cac9a63cc7489
Johnny Domino <jvdomino@gmail.com>
2017-05-29 23:50:52 -0400

    proper findOrCreate logic
    because why would things just work

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,5 +1,6 @@
 import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 const { inject: { service }} = Ember;
 
@@ -10,20 +11,38 @@ export default ToriiFirebaseAdapter.extend({
     const id = authorization.uid;
     const name = authorization.displayName;
     const email = authorization.email;
+    const store = this.get('store');
 
     // _always_ creating a record even when it exists smells bad
     // but it works just fine with Firebase...
     // in fact, trying to do a find() first and it fails
     // will then cause the subsequent create to fail because
     // of some bug in Ember Data that things the ID is already taken...
-    return this.get('store')
-      .createRecord('user', { id, name, email })
-      .save()
-      .then((currentUser) => { return { currentUser }; });
+    // return this.get('store')
+    //   .createRecord('user', { id, name, email })
+    //   .save()
+    //   .then((currentUser) => { return { currentUser }; });
+
+    return new RSVP.Promise(function(resolve) {
+      store
+        .find('user', id)
+        .then((currentUser) => { resolve({ currentUser }); })
+        .catch(() => {
+          // give Ember Data a chance to work out it's own issues
+          Ember.run.later(function() {
+            const currentUser = store.createRecord('user', { id, name, email });
+            currentUser.save();
+
+            resolve({ currentUser });
+          });
+        });
+    });
   },
 
   close() {
-    this._super(...arguments);
     this.get('store').unloadAll('user');
+    this.get('store').unloadAll('device');
+    this.get('store').unloadAll('schedule');
+    return this._super(...arguments);
   }
 });
