bab49f3529cb5de8a6c8f742d5960fa73fd2a242
Johnny Domino <jvdomino@gmail.com>
2017-06-10 16:05:24 -0400

    visualize cooling/heating modes

--- app/components/thermostat-ui.js
+++ app/components/thermostat-ui.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 const { computed, computed: { alias } } = Ember;
 
 export default Ember.Component.extend({
-  tagName: '',
-  classNames: ['thermostat-ui'],
+  classNames: ['thermostat-ui', 'layout-align-start-stretch', 'flex'],
+  classNameBindings: ['isHeating:heat', 'isCooling:cool'],
 
   model: alias('device.model'),
   serial: alias('device.serial'),
@@ -14,6 +14,9 @@ export default Ember.Component.extend({
   targetTemperature: alias('device.targetTemperature'),
   currentTemperature: alias('device.currentTemperature'),
   schedule: alias('device.schedule'),
+
+  isHeating: computed.equal('mode', 'heat'),
+  isCooling: computed.equal('mode', 'cool'),
 
   weatherIcon: computed('mode', function() {
     switch(this.get('device.mode')) {
