555cf7a6384c6d07529b81f84f9fbb51b0fdbe22
Edward Gaba <egaba@underarmour.com>
2018-03-25 15:25:10 -0700

    theme alerts

--- app/theme/service.js
+++ app/theme/service.js
@@ -1,8 +1,45 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  // toast message
   toastMessageText: null,
-  toastMessage(message) {
+  toastMessage(message, linkOptions = {}) {
     this.set('toastMessageText', message);
+
+    if (linkOptions.path && linkOptions.text) {
+      this.setProperties({
+        toastLinkText: linkOptions.text,
+        toastLinkPath:linkOptions.path,
+      });
+    }
+  },
+  clearToast() {
+    this.set('toastMessageText', '');
+  },
+
+  // alerts
+  alertDismissible: false,
+  setupConnectivityAlert: Ember.on('init', function() {
+    window.addEventListener('online', () => {
+      this.clearAlert();
+      this.toastMessage('re-connected');
+    });
+    window.addEventListener('offline', () => {
+      this.alertMessage('Your computer seems to be offline. You may continue to browse, but will not be able to checkout.', {
+        dismissible: true
+      });
+    });
+  }),
+  alertText: '',
+  alertContext: 'info',
+  alertMessage(message, options = {}) {
+    this.setProperties({
+      alertText: message,
+      alertContext: options.context || 'info',
+      alertDismissible: options.dismissible || false,
+    });
+  },
+  clearAlert() {
+    this.set('alertText', '');
   }
 });
