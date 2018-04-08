100c6d32f7644ceb821ddc3be797d7a3f34b8894
Edward Gaba <egaba@underarmour.com>
2018-04-08 15:50:40 -0700

    price totals

--- app/cart/controller.js
+++ app/cart/controller.js
@@ -2,5 +2,16 @@ import { alias } from '@ember/object/computed';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  totalPrice: Ember.computed('cartItemCount', function() {
+    const cartItemCount = this.get('cartItemCount') * 25
+    return `$${cartItemCount}.00`
+  }),
+  cartItemCount: Ember.computed('model.@each.quantity', function() {
+    if (this.get('model.length')) {
+      const quantities = this.get('model').mapBy('quantity');
+      return quantities.reduce((a, b) => parseInt(a, 10) + parseInt(b, 10));
+    }
 
+    return 0;
+  }),
 });
