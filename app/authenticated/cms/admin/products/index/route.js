1ab18f6cb1682974aacebafadb03217cf0d92bcd
Johnny Domino <jvdomino@gmail.com>
2018-04-21 13:47:36 -0700

    put a little hash in the cms.admin.products.index route

--- app/authenticated/cms/admin/products/index/route.js
+++ app/authenticated/cms/admin/products/index/route.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('product')
+    return RSVP.hash({
+      products: this.store.findAll('product')
+    });
   }
 });
